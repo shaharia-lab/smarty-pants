a4716fb3b885a268b67b20bbffbb43d198256954
Shaharia Azam <hello@shaharialab.com>
2024-09-08 15:22:59 +0200

    Now webpack can generate minified script as well

--- frontend/chat_widget/webpack.config.js
+++ frontend/chat_widget/webpack.config.js
@@ -1,45 +1,65 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'production';
+const baseConfig = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        library: 'ChatWidget',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+    },
+};
 
-    return {
-        entry: './src/index.tsx',
-        mode: isProduction ? 'production' : 'development',
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader', 'postcss-loader'],
-                },
-            ],
-        },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js'],
-        },
-        output: {
-            filename: 'chat-widget-bundle.js',
-            path: path.resolve(__dirname, 'dist'),
-            library: 'ChatWidget',
-            libraryTarget: 'umd',
-            globalObject: 'this',
-        },
-        optimization: {
-            minimizer: [new TerserPlugin({
-                extractComments: false,
-            })],
-        },
-        devServer: {
-            contentBase: path.join(__dirname, 'dist'),
-            compress: true,
-            port: 9000,
-        },
-    };
+const developmentConfig = {
+    ...baseConfig,
+    mode: 'development',
+    devtool: 'inline-source-map',
+    output: {
+        ...baseConfig.output,
+        filename: 'chat-widget.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+};
+
+const productionConfig = {
+    ...baseConfig,
+    mode: 'production',
+    devtool: 'source-map',
+    output: {
+        ...baseConfig.output,
+        filename: 'chat-widget.min.js',
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            extractComments: false,
+        })],
+    },
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'production') {
+        return [developmentConfig, productionConfig];
+    }
+    return developmentConfig;
 };
